ace856b85667e819c4022e5b686127dcb561f63a
Anand Choudhary <anandpc13@gmail.com>
2020-08-10 18:54:34 +0530

    few changes

--- Pirates.java
+++ Pirates.java
@@ -1,26 +1,25 @@
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 class Pirates {
     public static void main(String[] args) {
         int[] arr = {3, 6, 2, 7, 5};
-        System.out.println(pirates(arr));
+        System.out.println(pirateShips(arr));
     }
 
-    public static int pirates(int[] arr) {
-        LinkedList<Integer> linkedList = new LinkedList<>();
-        for (int value : arr) linkedList.add(value);
+    public static int pirateShips(int[] arr) {
+        List<Integer> ships = new ArrayList<>();
+        for (int loot : arr) ships.add(loot);
 
         int days = 0;
 
         while (true) {
             List<Integer> shipsToBeDestroyed = new ArrayList<>();
             // identify the ships which will be destroyed
-            for (int i = 1; i < linkedList.size(); i++) {
-                if (linkedList.get(i) > linkedList.get(i - 1)) {
-                    //System.out.println("days = " + days + " list = " + linkedList);
-                    shipsToBeDestroyed.add(linkedList.get(i));
+            for (int i = 1; i < ships.size(); i++) {
+                // current ship to be destroyed if it has more loot value then left one.
+                if (ships.get(i) > ships.get(i - 1)) {
+                    shipsToBeDestroyed.add(ships.get(i));
                 }
             }
 
@@ -29,10 +28,9 @@ class Pirates {
                 break;
             }
             // remove all identified ships which will be destroyed
-            for (Integer ships : shipsToBeDestroyed) {
-                linkedList.remove(ships);
+            for (Integer ship : shipsToBeDestroyed) {
+                ships.remove(ship);
             }
-            //System.out.println(linkedList);
             days++;
         }
 
